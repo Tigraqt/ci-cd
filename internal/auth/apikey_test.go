5d202f197e8e63c28fecf6a35ce2b5c3f0daa753
rolandCseh <csehroly@gmail.com>
2024-05-05 08:21:44 +0200

    Add unit tests

---
+++ internal/auth/apikey_test.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+// TestGetAPIKey tests various scenarios for GetAPIKey function
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name          string
+		headers       http.Header
+		want          string
+		expectedError error
+	}{
+		{
+			name:          "No Authorization Header",
+			headers:       http.Header{},
+			want:          "",
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization Header - Missing Token",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey"},
+			},
+			want:          "",
+			expectedError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Malformed Authorization Header - Incorrect Prefix",
+			headers: http.Header{
+				"Authorization": []string{"Bearer someapikey"},
+			},
+			want:          "",
+			expectedError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Correct Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey 1234"},
+			},
+			want:          "12345",
+			expectedError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.headers)
+			if err != nil && err.Error() != tt.expectedError.Error() {
+				t.Errorf("GetAPIKey() error = %v, expectedError %v", err, tt.expectedError)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
